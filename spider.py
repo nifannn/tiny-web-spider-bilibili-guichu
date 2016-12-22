f7ed1c50fa6aefb54d1057930b100a8cab264891
Fan Ni <ni.fan@outlook.com>
2016-12-22 14:37:25 +0800

    update

--- spider.py
+++ spider.py
@@ -15,10 +15,52 @@ def MysqlConn():
 	conn = pymysql.connect(**config)
 	return conn
 
+# get short url of sub categories
+def filterSub(url):
+	html = urlopen(url)
+	bsObj = BeautifulSoup(html, 'html.parser')
+	
+
+# get all page numbers of sub category
+def getPageNumber(sub):
+	pass
+
+# get url for spider
+def getSURLs():
+	url = 'http://www.bilibili.com/video/kichiku.html'
+	return [sub +  '#!page=' + str(page)
+	        for sub in filterSub(url)
+	        for page in getPageNumber(sub)]
+
+# get video id from url
+def filterAvIds(url):
+	pass
+
+# get video info from id
+def getAvInfo(AvId):
+	pass
+
+# update video info
+def updateAvInfo(AvInfo):
+	pass
+
+# get up id from mysql
+def selectUpIds():
+	pass
+
+# get up info from id
+def getUpInfo(UpId):
+	pass
+
+# update up info
+def updateUpInfo(UpInfo):
+	pass
+
 # scrap video info
 def spiderVideoInfo():
-	for url in getURLs():
-		for AvId in filterAvIds():
+	for surl in getSURLs():
+		url = 'http://www.bilibili.com/video/' + surl
+		for AvId in filterAvIds(url):
 			AvInfo = getAvInfo(AvId)
 			if AvInfo:
 				updateAvInfo(AvInfo)
@@ -34,3 +76,8 @@ def spiderUpInfo():
 
 	return 1
 
+def main(argv):
+	pass
+
+if __name__ == '__main__':
+	main(sys.argv)
